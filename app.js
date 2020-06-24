f053cb84417674ea77f549c9a92120ffafd49817
Robert Alestra <bobbyalestra135@gmail.com>
2020-06-24 00:00:04 -0400

    add an input bar and button with a little style to app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 //JS file please fucking kill me
+// yoooo mad funny - BA
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+
+sharesInput = document.createElement('input');
+sharesInput.id = 'stockShares';
+sharesInput.placeholder = "Enter How Many Shares You've Purchased";
+document.body.appendChild(sharesInput);
+
+sharesButton = document.createElement('button');
+sharesButton.id = 'sharesButton';
+sharesButton.innerText = 'Click here'
+document.body.appendChild(sharesButton)
+
+
+sharesButton = document.querySelector('#sharesButton');
+sharesButton.style.color = 'white';
+sharesButton.style.backgroundColor = 'blue';
+sharesButton.style.height = '40px'
+
+
+
+
+
+
+
+
+
+
+
+
+})
