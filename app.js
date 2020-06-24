c0a1007285b903877e36575a1eff06f48e0a5f18
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-06-24 00:34:58 -0400

    Chelsea's input change

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //JS file please fucking kill me
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    stockNameInput = document.createElement('input');
+    stockNameInput.id = 'stockName';
+    stockNameInput.placeholder = "Stock Name";
+    document.body.appendChild(stockNameInput);
+    
+    stockNameButton = document.createElement('button');
+    stockNameButton.id = 'stockNameButton';
+    stockNameButton.innerText = 'Submit';
+    document.body.appendChild(stockNameButton);
+
+})
