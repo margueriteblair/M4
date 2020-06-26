1ae5dc6eafe987f2b9eeec448b8ab422d8b24943
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-06-25 22:44:22 -0400

    attempting to fix

--- app.js
+++ app.js
@@ -41,12 +41,13 @@ document.addEventListener('DOMContentLoaded', () => {
   
     function stockReturn() {
         const apiKey1 = 'WDKZS0GMSWNAXG41';
+        let apiKey = '1833d3125cmsh0ab8c932d47a9a1p1baa09jsnd57107bb5df9';
         const ticker = document.getElementById(stockNameInput.id).value;
         // const interval = '5min';
         const functionName = 'TIME_SERIES_DAILY';
         // const outputSize = 'full';
         const url = `https://www.alphavantage.co/query?function=${functionName}&symbol=${ticker}&apikey=${apiKey1}`;
-    
+
         const xhr = new XMLHttpRequest();
     
         xhr.open('GET', url, true);
@@ -67,10 +68,80 @@ document.addEventListener('DOMContentLoaded', () => {
             let shares = document.getElementById(sharesInput.id).value;
             let profit = (shares*openPrice) - (shares*closePrice);
             
-            
             console.log(objectKey[0][0]); //date given
             html = `<p>Total Return For The Day:$${profit.toFixed(2)}</p>`;
             document.getElementById('totalReturn').innerHTML = html
+            
+            const data = null;
+            const array = [];
+    
+            const xhrAmazon = new XMLHttpRequest();
+            xhrAmazon.withCredentials = true;
+    
+            xhrAmazon.addEventListener("readystatechange", function () {
+                if (this.readyState === this.DONE) {
+                    let parsedResponseText = JSON.parse(this.responseText)
+                    console.log(parsedResponseText)
+                    console.log("___________________");
+    
+                    // Displays Amazon products list
+                    let outputData = '<ul>'
+                    let productImg = document.createElement("img")
+    
+                    for (let i = 0; i < parsedResponseText.products.length; i++) { //we will replace i < 1 with parsedResponseText after Jamie's part
+                        if (parsedResponseText.products[i].price <= profit) {
+    
+                            // entire list of Amazon items that are equal or less then money gained from stocks
+                            // console.log(parsedResponseText.products[i].title)
+                            // console.log(parsedResponseText.products[i].price)
+                            // console.log(i) // index of the Amazon item
+    
+                            array.push(i)
+                            console.log("___________________");
+                        }
+                    }
+                    console.log(`array length: ${array.length}`)
+    
+                    let arLength = array.length
+    
+                    let randomNumber = Math.floor((Math.random() * arLength))
+    
+                    console.log(`random number: ${randomNumber}`)
+    
+                    console.log(array)
+    
+                    let selectNum = array.splice(randomNumber, 1)
+    
+                    console.log(`value of spliced number: ${selectNum}`)
+    
+                    console.log(array)
+    
+                    // console.log(parsedResponseText.products[selectNum].title)
+                    // console.log(parsedResponseText.products[selectNum].price)
+                    // console.log('___________________')
+    
+                    outputData = outputData + `<li><p><a href=${parsedResponseText.products[selectNum].url}>${parsedResponseText.products[selectNum].title}</a></p></li><li><p>Price: $${parsedResponseText.products[selectNum].price}</p></li><br>`
+    
+                    productImg.src = parsedResponseText.products[selectNum].thumbnail;
+                    productImg.id = "productImg"
+    
+                    document.getElementById("amazonProducts").appendChild(productImg);
+    
+    
+                    outputData = outputData + '</ul>'
+                    document.getElementById('amazonProducts').innerHTML = outputData
+                    document.getElementById('amazonProducts').appendChild(productImg);
+                } else if (this.status === 404) {
+                    document.getElementById("amazonProducts").innerHTML = "404 ERROR: Data Not Found!"
+                }
+            });
+    
+            xhrAmazon.open("GET", "https://amazon-product-reviews-keywords.p.rapidapi.com/product/search?country=US&keyword=Lightning%20Deals");
+            xhrAmazon.setRequestHeader("x-rapidapi-host", "amazon-product-reviews-keywords.p.rapidapi.com");
+            xhrAmazon.setRequestHeader("x-rapidapi-key", apiKey);
+    
+            xhrAmazon.send(data);
+    
 
 
 
@@ -83,82 +154,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     // Begining of Amazon API
-    document.getElementById('sharesButton').addEventListener('click', loadText)
+    //document.getElementById('sharesButton').addEventListener('click', loadText)
 
-    function loadText() {
+    //function loadText() {
 
-        const data = null;
+        //const data = null;
         
 
-        const array = []
+        //const array = []
 
-        const xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+        
 
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === this.DONE) {
-                let parsedResponseText = JSON.parse(this.responseText)
-                console.log(parsedResponseText)
-                console.log("___________________");
-
-                // Displays Amazon products list
-                let outputData = '<ul>'
-                let productImg = document.createElement("img")
-
-                for (let i = 0; i < parsedResponseText.products.length; i++) { //we will replace i < 1 with parsedResponseText after Jamie's part
-                    if (parsedResponseText.products[i].price <= stockGain) {
-
-                        // entire list of Amazon items that are equal or less then money gained from stocks
-                        console.log(parsedResponseText.products[i].title)
-                        console.log(parsedResponseText.products[i].price)
-                        console.log(i) // index of the Amazon item
-
-                        array.push(i)
-                        console.log("___________________");
-                    }
-                }
-                console.log(`array length: ${array.length}`)
-
-                let arLength = array.length
-
-                let randomNumber = Math.floor((Math.random() * arLength))
-
-                console.log(`random number: ${randomNumber}`)
-
-                console.log(array)
-
-                let selectNum = array.splice(randomNumber, 1)
-
-                console.log(`value of spliced number: ${selectNum}`)
-
-                console.log(array)
-
-                console.log(parsedResponseText.products[selectNum].title)
-                console.log(parsedResponseText.products[selectNum].price)
-                // console.log('___________________')
-
-                outputData = outputData + `<li><p><a href=${parsedResponseText.products[selectNum].url}>${parsedResponseText.products[selectNum].title}</a></p></li><li><p>Price: $${parsedResponseText.products[selectNum].price}</p></li><br>`
-
-                productImg.src = parsedResponseText.products[selectNum].thumbnail;
-                productImg.id = "productImg"
-
-                document.getElementById("amazonProducts").appendChild(productImg);
-
-
-                outputData = outputData + '</ul>'
-                document.getElementById('amazonProducts').innerHTML = outputData
-                document.getElementById('amazonProducts').appendChild(productImg);
-            } else if (this.status === 404) {
-                document.getElementById("amazonProducts").innerHTML = "404 ERROR: Data Not Found!"
-            }
-        });
-
-        xhr.open("GET", "https://amazon-product-reviews-keywords.p.rapidapi.com/product/search?country=US&keyword=Lightning%20Deals");
-        xhr.setRequestHeader("x-rapidapi-host", "amazon-product-reviews-keywords.p.rapidapi.com");
-        xhr.setRequestHeader("x-rapidapi-key", apiKey);
-
-        xhr.send(data);
-
-    }
+    
 
 })
